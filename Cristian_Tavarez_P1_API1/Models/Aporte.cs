4d0d05d32adc165c28fbe10c0f1bf01cba8d1ac1
cristian tavarez <cristian22041@gmail.com>
2025-02-17 21:19:45 -0400

    Creando el Aporte

---
+++ Cristian_Tavarez_P1_API1/Models/Aporte.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cristian_Tavarez_P1_API1.Models
+{
+    public class Aporte
+    {
+        [Key] 
+        public int AporteId  { get; set; }
+
+        [Required(ErrorMessage = "Fecha es Requerida")]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "Concepto es Requerido")]
+        [StringLength(maximumLength: 5000, ErrorMessage = "Concepto muy largo")]
+        public string? Observacion { get; set; }
+
+        [Required(ErrorMessage = "Monto es Requerido")]
+        [Range(minimum: 0.01, maximum: double.MaxValue, ErrorMessage = "El Monto debe ser mayor a 0.01")]
+        public double Monto { get; set; }
+    }
+}
