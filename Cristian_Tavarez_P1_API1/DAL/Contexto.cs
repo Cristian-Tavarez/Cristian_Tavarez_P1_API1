b0b41755bb7e412097a1e04f1e0835a7eadc68f2
cristian tavarez <cristian22041@gmail.com>
2025-02-17 21:18:07 -0400

    Creacion del Contexto

--- Cristian_Tavarez_P1_API1/DAL/Contexto.cs
+++ Cristian_Tavarez_P1_API1/DAL/Contexto.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
-using RegistroTecnicos.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Cristian_Tavarez_P1_API1.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
 
-namespace RegistroTecnicos.DAL;
-
-public class Contexto : DbContext
+namespace Cristian_Tavarez_P1_API1.DAL
 {
-    public Contexto(DbContextOptions<Contexto> options)
-        : base(options) { }
+    public class Ingresos
+    {
+        [Key]
+        public int AporteID { get; set; }
 
-    public DbSet<Tecnicos> Tecnicos { get; set; }
+        [Required(ErrorMessage = "Fecha es Requerida")]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "Concepto es Requerido")]
+        [StringLength(maximumLength: 5000, ErrorMessage = "Concepto muy largo")]
+        public string? Concepto { get; set; }
+
+        [Required(ErrorMessage = "Monto es Requerido")]
+        [Range(minimum: 0.01, maximum: double.MaxValue, ErrorMessage = "El Monto debe ser mayor a 0.01")]
+        public double Monto { get; set; }
+    }
 }
